69143f559be499d2fb7bceefceed982bd1e144ce
acharls <ankitcharles@gmail.com>
2022-05-06 10:11:01 +0530

    remove duplicate from an sorted array

--- arrays/removeDuplicates.go
+++ arrays/removeDuplicates.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 func removeDuplicates(nums []int) []int {
 	size := len(nums)
 	i := 2
@@ -13,7 +14,6 @@ func removeDuplicates(nums []int) []int {
 	}
 	return nums
 }
-
 func main() {
 	i := []int{1, 1, 2, 2, 2, 3, 3}
 	//fmt.Println(removeDuplicates(i))
