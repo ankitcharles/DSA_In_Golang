845878caf1dff942cd6f426ec32a09452d288bf3
ankitcharles <30959262+ankitcharles@users.noreply.github.com>
2022-05-17 01:19:21 +0530

    sample golang code and optimized version of two sum in golang

--- arrays/removeDuplicateLeetcode26.go
+++ arrays/removeDuplicateLeetcode26.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func removeduplicates1(nums []int) int {
+	size := len(nums)
+	if size == 0 {
+		return size
+	}
+	i := 0
+	for j := 1; j < size; j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+func main() {
+	i := []int{1, 1, 2, 2, 2, 3, 3}
+	//fmt.Println(removeDuplicates(i))
+	fmt.Println(removeduplicates1(i))
 }
